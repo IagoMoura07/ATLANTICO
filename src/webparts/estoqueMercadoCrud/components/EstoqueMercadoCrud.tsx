1681c784137dcd794dedf012a8939ef99646f38b
Iago Pereira Moura e Souza <iagoperera@gmail.com>
2022-06-29 17:46:06 -0300

    first commit

--- src/webparts/estoqueMercadoCrud/components/EstoqueMercadoCrud.tsx
+++ src/webparts/estoqueMercadoCrud/components/EstoqueMercadoCrud.tsx
@@ -71,7 +71,7 @@ export default class EstoqueMercadoCrud extends React.Component<IEstoqueMercadoC
                   <span className={styles.label} onClick={this.getItemById}>Importar</span>
                 </div>
                 <div className={styles.button}>
-                  <span className={styles.label} onClick={this.getAllitens}>Ver todos</span>
+                  <span className={styles.label} onClick={this.getAllitems}>Ver todos</span>
                 </div>
                 <div className={styles.button}>
                   <span className={styles.label} onClick={this.updateItem}>Atualizar</span>
@@ -87,16 +87,18 @@ export default class EstoqueMercadoCrud extends React.Component<IEstoqueMercadoC
     );
   }
   //Criar item
-  //Teste GIT
 
   private createItem = async () => {
-    var nome_produto = document.getElementById("NOME_PRODUTO")['value'];
-    var quantidade = document.getElementById("QUANTIDADE")['value'];
-    var data = document.getElementById("DATA_VALIDADE")['value'];
+    var nome_produto = document.getElementById("NOME_PRODUTO")['value']; 
+    var quantidade = document.getElementById("QUANTIDADE")['value']; 
+    var data = document.getElementById("DATA_VALIDADE")['value']; 
     var tipo_produto = document.getElementById("TIPO_PRODUTO")['value'];
     var produto_importado = document.getElementById("PRODUTO_IMPORTADO")['value'];
 
-
+    //Converte a data para padrão BR
+    var data_br = new Date(data);
+    var data_formatada = data_br.toLocaleDateString('pt-BR', {timeZone: 'UTC'});
+    //Verifica campos vazios
     if (produto_importado === "") {
       alert("O campo Produto importado e diferente de SIM ou NAO");
 
@@ -118,26 +120,24 @@ export default class EstoqueMercadoCrud extends React.Component<IEstoqueMercadoC
       alert("O campo Tipo de produto esta vazio");
 
     }
-
-    /*document.getElementById("PRODUTO_IMPORTADO" && "NOME_PRODUTO" && "QUANTIDADE" && "DATA_VALIDADE" && "TIPO_PRODUTO")['value'] != ""*/
+    
+  
     try {
-      /*console.log(nome_produto);
-      console.log(quantidade);
-      console.log(data);
-      console.log(tipo_produto);
-      console.log(produto_importado);*/
 
 
+      
       if (nome_produto.trim() && quantidade && data && tipo_produto && produto_importado != "") {
-        const addItem = await sp.web.lists.getByTitle("SUPERMERCADO").items.add({
-          'NOME_PRODUTO': document.getElementById("NOME_PRODUTO")['value'],
-          'QUANTIDADE': document.getElementById("QUANTIDADE")['value'],
-          'PRODUTO_IMPORTADO': document.getElementById("PRODUTO_IMPORTADO")['value'],
-          'DATA_VALIDADE': document.getElementById("DATA_VALIDADE")['value'],
-          'TIPO_PRODUTO': document.getElementById("TIPO_PRODUTO")['value']
+        //Imput de dados via na lista
+        const addItem = await sp.web.lists.getByTitle("SUPERMERCADO").items.add({ //Lista utilizada SUPERMERCADO - Altera para nome da lista usada
+          'NOME_PRODUTO': nome_produto,
+          'QUANTIDADE': quantidade,
+          'PRODUTO_IMPORTADO': produto_importado,
+          'DATA_VALIDADE': data_formatada,
+          'TIPO_PRODUTO': tipo_produto,
+          'DATA_SEM_FORMATACAO':data
         });
 
-
+        //Limpa os campos do formulario
         console.log(addItem);
         alert(`Item adicionado com sucesso. ID: ${addItem.data.ID}`);
         document.getElementById("NOME_PRODUTO")['value'] = "";
@@ -161,13 +161,15 @@ export default class EstoqueMercadoCrud extends React.Component<IEstoqueMercadoC
   //Buscar por ID
   private getItemById = async () => {
     try {
+      //Seleciona ID do item digitado
       const id: number = document.getElementById('itemId')['value'];
+      
       if (id > 0) {
-        const item: any = await sp.web.lists.getByTitle("SUPERMERCADO").items.getById(id).get();
+        const item: any = await sp.web.lists.getByTitle("SUPERMERCADO").items.getById(id).get(); //Busca na lista pelo ID
         document.getElementById('NOME_PRODUTO')['value'] = item.NOME_PRODUTO;
         document.getElementById('QUANTIDADE')['value'] = item.QUANTIDADE;
         document.getElementById('PRODUTO_IMPORTADO')['value'] = item.PRODUTO_IMPORTADO;
-        document.getElementById('DATA_VALIDADE')['value'] = item.DATA_VALIDADE;
+        document.getElementById('DATA_VALIDADE')['value'] = item.DATA_SEM_FORMATACAO;
         document.getElementById('TIPO_PRODUTO')['value'] = item.TIPO_PRODUTO;
       }
       else {
@@ -181,17 +183,18 @@ export default class EstoqueMercadoCrud extends React.Component<IEstoqueMercadoC
 
 
   //Buscar todos os itens
-  private getAllitens = async () => {
+  private getAllitems = async () => {
     try {
-      const itens: any[] = await sp.web.lists.getByTitle("SUPERMERCADO").items.get();
-      console.log(itens);
-      if (itens.length > 0) {
+      const items: any[] = await sp.web.lists.getByTitle("SUPERMERCADO").items.get();
+      console.log(items);
+      if (items.length > 0) {
+        //Gera uma variavel com o HTML da tabela contendo o conteudo da lista 
         var html = `<table width="100px" border = "1" cellspacing="2" cellpadding="4"><tr align="center"><th width="250px">ID</th><th>Nome Produto</th><th>Quantidade    </th> <th>Produto Importado    </th> <th>Data de Validade    </th> <th>Tipo do Produto    </th></tr>`;
-        itens.map((item, index) => {
+        items.map((item, index) => {
           html += `<tr align="center"><td>${item.ID}      </td><td>${item.NOME_PRODUTO}      </td><td>${item.QUANTIDADE}      </td> <td>${item.PRODUTO_IMPORTADO}      </td> <td>${item.DATA_VALIDADE}      </td> <td>${item.TIPO_PRODUTO}      </td></li>`;
         });
         html += `</table>`;
-        document.getElementById("allItems").innerHTML = html;
+        document.getElementById("allitems").innerHTML = html;
       } else {
         alert(`Lista está vazia`);
       }
@@ -211,6 +214,7 @@ export default class EstoqueMercadoCrud extends React.Component<IEstoqueMercadoC
     var produto_importado = document.getElementById("PRODUTO_IMPORTADO")['value'];
 
 
+    //Verifica campos vazios
     if (produto_importado === "") {
       alert("O campo Produto importado e diferente de SIM ou NAO");
 
@@ -232,18 +236,21 @@ export default class EstoqueMercadoCrud extends React.Component<IEstoqueMercadoC
       alert("O campo Tipo de produto esta vazio");
 
     }
-
+    var data_br = new Date(data);
+    var data_formatada = data_br.toLocaleDateString('pt-BR', {timeZone: 'UTC'});
     try {
       if (nome_produto.trim() && quantidade && data && tipo_produto && produto_importado != "") {
         const id: number = document.getElementById('itemId')['value'];
         if (id > 0) {
+          //Imput de dados via na lista
           const itemUpdate = await sp.web.lists.getByTitle("SUPERMERCADO").items.getById(id).update({
-            'NOME_PRODUTO': document.getElementById("NOME_PRODUTO")['value'],
-            'QUANTIDADE': document.getElementById("QUANTIDADE")['value'],
-            'PRODUTO_IMPORTADO': document.getElementById("PRODUTO_IMPORTADO")['value'],
-            'DATA_VALIDADE': document.getElementById("DATA_VALIDADE")['value'],
-            'TIPO_PRODUTO': document.getElementById("TIPO_PRODUTO")['value']
+            'NOME_PRODUTO': nome_produto,
+            'QUANTIDADE': quantidade,
+            'PRODUTO_IMPORTADO': produto_importado,
+            'DATA_VALIDADE': data_formatada,
+            'TIPO_PRODUTO': tipo_produto
           });
+          //Limpa os campos do formulario
           console.log(itemUpdate);
           alert(`Item com ID: ${id} Alterado com sucesso`);
           document.getElementById("NOME_PRODUTO")['value'] = "";
@@ -251,6 +258,7 @@ export default class EstoqueMercadoCrud extends React.Component<IEstoqueMercadoC
           document.getElementById("PRODUTO_IMPORTADO")['value'] = "";
           document.getElementById("DATA_VALIDADE")['value'] = "";
           document.getElementById("TIPO_PRODUTO")['value'] = "";
+          document.getElementById("itemId")['value'] = "";
         }
         else {
           alert(`Por favor insira um ID Valido`);
@@ -272,6 +280,13 @@ export default class EstoqueMercadoCrud extends React.Component<IEstoqueMercadoC
         let deleteItem = await sp.web.lists.getByTitle("SUPERMERCADO").items.getById(id).delete();
         console.log(deleteItem);
         alert(`Item ID: ${id} apagado com sucesso`);
+        document.getElementById("NOME_PRODUTO")['value'] = "";
+        document.getElementById("QUANTIDADE")['value'] = "";
+        document.getElementById("PRODUTO_IMPORTADO")['value'] = "";
+        document.getElementById("DATA_VALIDADE")['value'] = "";
+        document.getElementById("TIPO_PRODUTO")['value'] = "";
+        document.getElementById("itemId")['value'] = "";
+        
       }
       else {
         alert(`Por favor insira um ID Valido`);
